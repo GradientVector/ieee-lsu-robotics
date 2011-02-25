11d842dc0a43a66d268ae047eada99eb2c6926d9
aelias3@yahoo.com <aelias3@yahoo.com@4ffe7329-c24e-311f-73c9-120b4c3aae99>
2011-02-25 23:00:04 +0000

    new files created. before editing them, before to check them out via the wiki page

---
+++ trunk/src/main.cpp
@@ -0,0 +1,21 @@
+/**
+This is the main program that will be run by the robot.
+
+LOG:
+	2011 Feb 24 - created by Andrew Elias
+
+TODO list:
+	fill in the main() function
+
+*/
+
+#include "vision.h"
+#include "execution.h"
+#include "navigation.h"
+#include "variousInputs.h"
+#include "vision.h"
+using namespace std;
+
+int main() {
+	//TODO
+}
---
+++ trunk/src/vision.h
@@ -0,0 +1,62 @@
+/**
+This, along with vision.cpp, contain all functionality necessary for using the camera.
+
+LOG:
+	2011 Feb 24 - blank document created by Andrew Elias
+
+TODO notes:
+	
+
+ */
+
+#ifndef _VISION_H_
+#define _VISION_H_
+
+
+using namespace std;
+
+//holds a location in an image; units are pixels.
+//the origin is the upper lefthand side of the image.
+class PixelPoint {
+	public:
+		int x;	//x pixel coordinate, defining x=0 to be the ?top? of image
+		int y; 	//y pixel coordinate, defining y=0 to be the left of image
+};
+
+
+
+
+//holds an absolute direction. the value is protected, so that it never leaves the range 0-360 (degrees).
+//0 degrees is defined to be in the direction of the map's positive x axis (pointing to the right/east). 
+class Angle {
+	private:
+		double angle; 	//holds values from 0 to 360
+	public:
+		double getAngle();
+		void setAngle(double newAngle);
+};
+
+
+const int NOT_FOUND = 0; 
+const int RED       = 1;
+const int GREEN     = 2;
+const int BLUE      = 3;
+const int YELLOW    = 4;
+const int BLACK     = 5;
+
+class Cylinder {
+	public:
+		CartesianPoint location;
+		int color;
+};
+
+
+//uses the image to find the pixel coordinates of the base of a cylinder (or line)
+PixelPoint findObject(Cylinder cyl);
+
+//converts the pixel coordinates to a real-life relative position
+PolarPosition placeObject(PixelPoint);
+
+
+
+#endif
