cbebfaecfdb7224506403986ee00398ead6d1d49
aelias3@yahoo.com <aelias3@yahoo.com@4ffe7329-c24e-311f-73c9-120b4c3aae99>
2011-03-04 21:00:00 +0000

    asdf

--- trunk/src/classes/Bot.h
+++ trunk/src/classes/Bot.h
@@ -2,6 +2,7 @@
 Filename:	Bot.h
 
 Bot class. represents our robot. holds the location and angle info.
+Also includes "execution" methods for talking to the motor controller.
 Will only be instantiated once.
 
 author:		Andrew Elias
@@ -16,4 +17,6 @@ class Bot {
 	public:
 		CartesianPoint location;
 		Angle angle;
+		void setVel(double vel);
+		void setRotVel(double rotVel);
 };
