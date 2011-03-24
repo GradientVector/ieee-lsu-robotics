84824ed44da6a21891a075f884ef222f293e2d5a
aelias3@yahoo.com <aelias3@yahoo.com@4ffe7329-c24e-311f-73c9-120b4c3aae99>
2011-03-24 23:15:33 +0000

    broke down the navigation.h/cpp into multiple separate classes

--- trunk/src/nav/navigation.h
+++ trunk/src/nav/navigation.h
@@ -1,12 +1,3 @@
-/**
-Filename:	navigation.h
-	
-This, along with navigation.cpp, hold instructions for movement and rough geographical positioning.
-
-Log:	2011 Feb 25 - document created by Andrew Elias
-	2011 Mar 18 - driveToMainLine() added by Andrew Elias
-
-*/
 
 #ifndef _NAVIGATION_H_
 #define _NAVIGATION_H_
@@ -15,79 +6,13 @@ Log:	2011 Feb 25 - document created by Andrew Elias
 #include "../vision/vision.h"
 
 
-//will only be instantiated once. represents the robot itself.
-//Holds location and angle information.
-class Bot {
-	private:
-	double velocity, rotVel;
-	
-	public:
-	const double STD_STOP_DIST = 6; 	//when navigating by homeInOn(-,-),  this is the standard stopping distance(inches) when we don't want to touch the object 
-	const double TOUCH_DIST = 0;		//distance(inches) from our camera to the object when we are touching it
-	const double WHL_DIAM;			//separation b/t the wheels
-	
-	//high speeds at which things start going wrong. (temp; subj to change)
-	const double MAX_SPEED = 10;		//inches/second
-	const double MAX_ROT_SPEED = 720;	//degrees/second
-	//good, standard speeds that aren't too fast/slow. on a scale from -1 to 1, where 1 is MAX_SPEED
-	const double COMFY_SPEED = 0.5;		
-	const double COMFY_TURN_SPEED = 0.5;
-	
-	SensorSet sensors;
-	CartesianPoint location;
-	Angle angle;
-	MCI mci;
-
-	void driveToMainLine();
-	void followLineTo(Cylinder cyl, double distance);
-	void pointTo(Cylinder cyl);
-	void homeInOn(Cylinder, double distance);
-	void chargeFor(/*TODO*/);
-	void dischargeFor(/*TODO*/);
-	void getVel();
-	void setVel(double vel);
-	void getRotVel();
-	void setRotVel(double rotVel);
-};
 
 
-//a list of cylinders (and their colors/locations)
-class Map {
-	public:
-	Cylinder yellowCyl;
-	Cylinder blueCyl;
-	Cylinder greenCyl;
-	Cylinder redCyl;
-	//TODO: line?
-	Map();
-}
 
 
-//holds a location on a map; units are inches.
-//by convention the origin is the lower lefthand side of the map.
-class CartesianPoint {
-	public:
-	double x;	//units: inches
-				//x==0 indicates the bottom of the map
-	double y;
-				//y==0 indicates the left side of the map
-	CartesianPoint(double xPassed, double yPassed);
-};
 
 
-//will be used to hold a relative position, in polar coordinates.
-//This will help us know what angle we need to turn to face an object.
-class PolarPoint {
-	public:
-	double r;	//radial  coordinate. units: inches
-			//r==0 indicates our (the robot's) position
-	Angle th;	//angular coordinate. units: degrees
-			//th==0 indicates directly ahead of us
-			//note: 'th' indicates 'theta'
-        PolarPoint(double in_r, Angle in_th);
-        PolarPoint(double in_r, double in_th);
-        PolarPoint();
-};
+
 
 
 
--- trunk/src/vision/vision.h
+++ trunk/src/vision/vision.h
@@ -71,14 +71,6 @@ const int BLUE      = 3;
 const int YELLOW    = 4;
 const int BLACK     = 5;
 
-const double VERTICAL_TILT_ANGLE = 90;   // 0 degrees is pointing perpendicular (straight at) the ground, 90 degrees is parallel to the ground
-const double CAMERA_HEIGHT = 5;          // Height (in inches) from ground. Test value of 5 was randomly chosen.
-const double VERT_FIELD_ANGLE = 45;      // 56 deg or 75 deg, probably using 75 deg
-const double HORIZ_FIELD_ANGLE = 60;
-const int IMG_WIDTH_PX = 640;
-const int IMG_HEIGHT_PX =  480;
-const double Y_SCALING_RATIO;            // How much stuff strinks while moving away
-
 
 class Cylinder {
 	public:
