4fdc24261f3b6ba13c0b737420a84b0feb648c3a
aelias3@yahoo.com <aelias3@yahoo.com@4ffe7329-c24e-311f-73c9-120b4c3aae99>
2011-04-10 01:33:35 +0000

    changed some hard tabs to soft tabs

--- trunk/src/nav/Bot.h
+++ trunk/src/nav/Bot.h
@@ -9,82 +9,89 @@
 //will only be instantiated once. represents the robot itself.
 //Holds location and angle information.
 class Bot {
-	private:
-	double velocity, rotVel;      //rotVel>0 means the bot is twisting left
+    private:
+    double velocity, rotVel;      //rotVel>0 means the bot is twisting left
 
-	//accessors for each wheel speed due to each type of motion; returns value from -9999 to 9999 (mci scale)
-	double getRSpeedDueToVel();
-	double getLSpeedDueToVel();
-	double getRSpeedDueToRotVel();
-	double getLSpeedDueToRotVel();
+    //accessors for each wheel speed due to each type of motion; returns value from -9999 to 9999 (mci scale)
+    double getRSpeedDueToVel();
+    double getLSpeedDueToVel();
+    double getRSpeedDueToRotVel();
+    double getLSpeedDueToRotVel();
 	
 	
-	public:
-	const double STD_STOP_DIST = 6; 	//when navigating by homeInOn(-,-),  this is the standard stopping distance(inches) when we don't want to touch the object 
-	const double TOUCH_DIST = 0;		//distance(inches) from our camera to the object when we are touching it
-	const double WHL_DIAM;			//separation b/t the wheels
-	const double WHL_RADIUS = WHL_DIAM/2;	
+    public:
+    const double STD_STOP_DIST = 6;     //when navigating by homeInOn(-,-),  this is the standard stopping distance(inches) when we don't want to touch the object 
+    const double TOUCH_DIST = 0;        //distance(inches) from our camera to the object when we are touching it
+    const double WHL_DIAM;              //separation b/t the wheels
+    const double WHL_RADIUS = WHL_DIAM/2;	
 
-	//high speeds at which things start going wrong(i.e. slippage, popping wheelies, etc). Subj to change
-	const double MAX_SPEED = 25;		//inches/second
-	const double MAX_ROT_SPEED = 400;	//degrees/second
+    //high speeds at which things start going wrong(i.e. slippage, popping wheelies, etc). Subj to change
+    const double MAX_SPEED     = 25;    //inches/second
+    const double MAX_ROT_SPEED = 400;	//degrees/second
 
-	//good, standard speeds that aren't too fast/slow.
-	const double COMFY_SPEED = 0.5*MAX_SPEED;		
-	const double COMFY_TURN_SPEED = 0.5*MAX_ROT_SPEED;
+    //good, standard speeds that aren't too fast/slow.
+    const double COMFY_SPEED      = 0.5 * MAX_SPEED;		
+    const double COMFY_TURN_SPEED = 0.5 * MAX_ROT_SPEED;
 
-	const double IN_PER_SEC = 5;   //the speed, in in/sec, represented by passing "1,1" to the MCI's setVelocity() function; a simple unit conversion factor. TODO: value should be changed later
-	const double DEG_PER_SEC = 5;		//the rotational speed, in deg/sec, represented by passing "-1,1" (TODO) to the MCI's setVelocity() function; a simple unit conversion factor. TODO: value should be changed later
+    const double IN_PER_SEC = 5;        //the speed, in in/sec, represented by passing "1,1" to the MCI's setVelocity() function; a simple unit conversion factor. TODO: value should be changed later
+    const double DEG_PER_SEC = 5;       //the rotational speed, in deg/sec, represented by passing "-1,1" (TODO) to the MCI's setVelocity() function; a simple unit conversion factor. TODO: value should be changed later
 
-        // Movement logic
-        const bool LEFT = true;                 // Turning left uses positive velocity
-	const bool RIGHT =  !LEFT;              // Turning right uses negative velocity
-        const bool FORWARD = true;              // Moving forward uses positive velocity
+    // Movement logic
+    const bool LEFT = true;                 // Turning left uses positive velocity
+    const bool RIGHT =  !LEFT;              // Turning right uses negative velocity
+    const bool FORWARD = true;              // Moving forward uses positive velocity
 	const bool BACKWARD =  !FORWARD;        // Moving backward uses negative velocity
 
-        // Camera related variables
-	const double VERTICAL_TILT_ANGLE = 90;   // 0 degrees is pointing perpendicular (straight at) the ground; 90 degrees is parallel to the ground
-	const double CAMERA_HEIGHT = 5;          // Height (in inches) from ground. Test value of 5 was randomly chosen.
-	const double VERT_FIELD_ANGLE = 45;      // 56 deg or 75 deg, probably using 75 deg
-	const double HORIZ_FIELD_ANGLE = 60;
-	const int IMG_WIDTH_PX = 640;
-	const int IMG_HEIGHT_PX =  480;
+    // Camera related variables
+    const double VERTICAL_TILT_ANGLE = 90;   // 0 degrees is pointing perpendicular (straight at) the ground; 90 degrees is parallel to the ground
+    const double CAMERA_HEIGHT = 5;          // Height (in inches) from ground. Test value of 5 was randomly chosen.
+    const double VERT_FIELD_ANGLE = 45;      // 56 deg or 75 deg, probably using 75 deg
+    const double HORIZ_FIELD_ANGLE = 60;
+    const int IMG_WIDTH_PX = 640;
+    const int IMG_HEIGHT_PX =  480;
 
-	SensorSet sensors;
-	CartesianPoint location;
-	Angle angle;
-	MCI mci;
+    SensorSet sensors;
+    CartesianPoint location;
+    Angle angle;
+    MCI mci;
 
-	void driveToMainLine();
-	void followLineTo(Cylinder cyl, double distance);
-	void pointTo(Cylinder cyl);
-	void homeInOn(Cylinder, double distance);
-	void chargeFor(double time);
-	void dischargeFor(double time);
-	void getVel();
-	void setVel(double vel);
-	void getRotVel();
-	void setRotVel(double rotVel);
-        PixelPoint searchFor(Cylinder cyl);
-        // Start move fuctions (forwards/backwards)
-        void moveTo(bool direction, double distance, double speed);
-        void startMoving(bool direction, double speed);
-        void moveForwardTo(double distance, double speed);
-        void moveBackwardTo(double distance, double speed);
-        void startMovingForward(double speed);
-        void startMovingBackward(double speed);
-        void stopMoving();
-        // Start turning functions (rotate left/right)
-        void turnTo(double angle, double speed);
-        void turnTo(Angle angle, double speed);
-        void turn(bool direction, double angle, double speed);
-        void turn(bool direction, Angle angle, double speed);
-        void startTurning(bool direction, double speed);
-        void turnLeft(double angle, double speed);
-        void turnRight(double angle, double speed);
-        void startTurningLeft(double speed);
-        void startTurningRight(double speed);
-        void stopTurning();
+    void driveToMainLine();
+    void followLineTo(Cylinder cyl, double distance);
+
+    //Cylinder Following
+    void pointTo(Cylinder cyl);
+    PixelPoint searchFor(Cylinder cyl);
+    void homeInOn(Cylinder, double distance);
+
+    //Chargin/Discharging
+    void chargeFor(double time);
+    void dischargeFor(double time);
+
+    //Vel / RotVel interfacing
+    void getVel();
+    void setVel(double vel);
+    void getRotVel();
+    void setRotVel(double rotVel);
+
+    // Start move fuctions (forwards/backwards)
+    void moveTo(bool direction, double distance, double speed);
+    void startMoving(bool direction, double speed);
+    void moveForwardTo(double distance, double speed);
+    void moveBackwardTo(double distance, double speed);
+    void startMovingForward(double speed);
+    void startMovingBackward(double speed);
+    void stopMoving();
+    // Start turning functions (rotate left/right)
+    void turnTo(double angle, double speed);
+    void turnTo(Angle angle, double speed);
+    void turn(bool direction, double angle, double speed);
+    void turn(bool direction, Angle angle, double speed);
+    void startTurning(bool direction, double speed);
+    void turnLeft(double angle, double speed);
+    void turnRight(double angle, double speed);
+    void startTurningLeft(double speed);
+    void startTurningRight(double speed);
+    void stopTurning();
 };
 
 #endif	//BOT_H
