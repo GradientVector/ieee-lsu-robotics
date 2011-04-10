d9adb3469e64451212ebb2e82d253b1df4eadfca
hmasson4@gmail.com <hmasson4@gmail.com@4ffe7329-c24e-311f-73c9-120b4c3aae99>
2011-04-10 01:01:19 +0000

    added comments to constants

--- src/nav/Bot.h
+++ src/nav/Bot.h
@@ -36,12 +36,13 @@ class Bot {
 	const double IN_PER_SEC = 5;   //the speed, in in/sec, represented by passing "1,1" to the MCI's setVelocity() function; a simple unit conversion factor. TODO: value should be changed later
 	const double DEG_PER_SEC = 5;		//the rotational speed, in deg/sec, represented by passing "-1,1" (TODO) to the MCI's setVelocity() function; a simple unit conversion factor. TODO: value should be changed later
 
-     const bool LEFT = true;                 // Turning left uses positive velocity
+        // Movement logic
+        const bool LEFT = true;                 // Turning left uses positive velocity
 	const bool RIGHT =  !LEFT;              // Turning right uses negative velocity
+        const bool FORWARD = true;              // Moving forward uses positive velocity
+	const bool BACKWARD =  !FORWARD;        // Moving backward uses negative velocity
 
-     const bool FORWARD = true;                 // Moving forward uses positive velocity
-	const bool BACKWARD =  !FORWARD;           // Moving backward uses negative velocity
-
+        // Camera related variables
 	const double VERTICAL_TILT_ANGLE = 90;   // 0 degrees is pointing perpendicular (straight at) the ground; 90 degrees is parallel to the ground
 	const double CAMERA_HEIGHT = 5;          // Height (in inches) from ground. Test value of 5 was randomly chosen.
 	const double VERT_FIELD_ANGLE = 45;      // 56 deg or 75 deg, probably using 75 deg
