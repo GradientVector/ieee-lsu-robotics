cacdf1ddf718661a63808bd2d6563011bb8f9f98
aelias3@yahoo.com <aelias3@yahoo.com@4ffe7329-c24e-311f-73c9-120b4c3aae99>
2011-04-12 16:43:23 +0000

    util now compiles

--- src/util/util.cpp
+++ src/util/util.cpp
@@ -1,7 +1,8 @@
 
+#include "util.h"
 
-//TODO: check this; it may have to be edited, if sleep(...) suspends too much of the bot's execution
-//Input: time is in milliseconds
-void wait(int milliseconds) {
-	sleep( ((double)milliseconds)/1000 );   //the "/1000" is because sleep() takes parameters in seconds ?TODO
+void wait(unsigned int milliseconds)
+{
+    clock_t goal = milliseconds + clock();
+    while (goal > clock());
 }
