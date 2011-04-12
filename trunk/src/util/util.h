ded410e613068dad7b169858200090f3a2498732
aelias3@yahoo.com <aelias3@yahoo.com@4ffe7329-c24e-311f-73c9-120b4c3aae99>
2011-04-12 16:43:23 +0000

    util now compiles

--- trunk/src/util/util.h
+++ trunk/src/util/util.h
@@ -5,10 +5,13 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+//#include <unistd.h>
+//#include <cstdlib>
+//#include <stdlib.h>
+//#include <ctime>  //may be necessary if we change the wait() function later
+#include <time.h>
 
-#include <ctime>  //may be necessary if we change the wait() function later
-
-void wait(int milliseconds);
+void wait(unsigned int milliseconds);
 
 const double PI = 3.1415926;
 const double RADIANS_PER_DEGREE = PI / 180;
