7977c65f83ff67df36fa7728cc124b7904604043
hmasson4@gmail.com <hmasson4@gmail.com@4ffe7329-c24e-311f-73c9-120b4c3aae99>
2011-04-12 04:08:20 +0000

    added functions to find black line on ground and plot midline to follow. also added include lines for opencv to vision.cpp

--- trunk/src/vision/vision.h
+++ trunk/src/vision/vision.h
@@ -6,13 +6,19 @@ loose functions for vision, not belonging to a class
 #define _VISION_H_
 
 using namespace std;
+using namespace cv;
 
 //uses the image to find the pixel coordinates of the base of a cylinder (or line)
 PixelPoint findObject(Cylinder cyl);
 
 //converts the pixel coordinates to a real-life relative position
-PolarPoint placeObject(PixelPoint);
+PolarPoint placeObject(PixelPoint object);
 
-
+void grabLines(Mat &img, vector<Vec4i> &line);
+void getMidLine(Point line1_pt1, Point line1_pt2, Point line2_pt1, Point line2_pt2, Point &mdpt1, Point &midpt2);
+Point getMidpoint(Point pt1, Point pt2);
+double getDistance(Point pt1, Point pt2);
+PixelPoint findLine();
+//#TODO: Need to update this header file, missing some functions
 
 #endif
