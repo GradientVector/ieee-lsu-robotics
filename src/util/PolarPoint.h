f77a5eb7a8be945d3c2fc62e1705cbf5100d7fe5
aelias3@yahoo.com <aelias3@yahoo.com@4ffe7329-c24e-311f-73c9-120b4c3aae99>
2011-04-13 21:15:51 +0000

    ?

--- src/util/PolarPoint.h
+++ src/util/PolarPoint.h
@@ -1,21 +1,26 @@
 #ifndef POLAR_POINT_H
 #define POLAR_POINT_H
 
-#include "Angle.h"
 
 //will be used to hold a relative position, in polar coordinates.
 //This will help us know what angle we need to turn to face an object.
 class PolarPoint {
 	public:
+    //TODO: make r, th private
 	double r;	//radial  coordinate. units: inches
 			//r==0 indicates our (the robot's) position
-	Angle th;	//angular coordinate. units: degrees
+    void   setR(double toSet);
+    double getR();
+
+	double th;	//angular coordinate. units: degrees
 			//th==0 indicates directly ahead of us
 			//note: 'th' indicates 'theta'
-        PolarPoint(double in_r, Angle in_th);
-        PolarPoint(double in_r, double in_th);
-        PolarPoint();
-        void setPolarPoint(double in_r, double in_th);
+    void   setTh(double toSet);
+    double getTh();
+
+    PolarPoint(double in_r, double in_th);
+    PolarPoint();
+    void setPolarPoint(double in_r, double in_th);
 };
 
 #endif
