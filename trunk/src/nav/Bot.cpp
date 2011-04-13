717a064ec62da8a579db955d3c43a99fbd0c9bcc
aelias3@yahoo.com <aelias3@yahoo.com@4ffe7329-c24e-311f-73c9-120b4c3aae99>
2011-04-13 19:42:13 +0000

    changed some Bot const member variables into 'getter' functions, b/c it won't compile the other way

--- trunk/src/nav/Bot.cpp
+++ trunk/src/nav/Bot.cpp
@@ -1,7 +1,7 @@
 
 #include "Bot.h"
 
-Bot::Bot() STD_STOP_DIST(6), TOUCH_DIST(0), WHL_DIAM(6.6), MAX_SPEED(25), MAX_ROT_SPEED(400), IN_PER_SEC(5), DEG_PER_SEC(5)
+Bot::Bot() : STD_STOP_DIST(6), TOUCH_DIST(0), WHL_DIAM(6.6), MAX_SPEED(25), MAX_ROT_SPEED(400), IN_PER_SEC(5), DEG_PER_SEC(5)
 {
     //intentionally blank
 }
@@ -9,11 +9,11 @@ Bot::Bot() STD_STOP_DIST(6), TOUCH_DIST(0), WHL_DIAM(6.6), MAX_SPEED(25), MAX_RO
 //Will only be used once. Gets us from the starting point (facing 'eastward') 
 //to the T-junction of the black lines, facing 'northward'. After that, we can follow the right line. 
 void Bot::driveToMainLine() {
-   setVel(COMFY_SPEED);
-   wait((18/COMFY_SPEED)*1000);		//*1000, converting s to ms
+   setVel(getCOMFY_SPEED());
+   wait((18/getCOMFY_SPEED())*1000);		//*1000, converting s to ms
    setVel(0);
-   setRotVel(COMFY_TURN_SPEED);
-   wait((90/COMFY_TURN_SPEED)*1000);	//*1000, converting s to ms
+   setRotVel(getCOMFY_TURN_SPEED());
+   wait((90/getCOMFY_TURN_SPEED())*1000);	//*1000, converting s to ms
    setRotVel(0);
 }
 
@@ -28,10 +28,10 @@ void Bot::goToLineAndFollowItTo(Cylinder cyl, double distanceToTravel) {
     PhysicalLine physicalLine = ??; //TODO
 
 
-    if(/*TODO: "we're not on the line" */) {
-        getOn(/* TODO: the line*/);  
+    if(TODO: "we're not on the line" ) {
+        getOn( TODO: the line);  
         pointTo(cyl);   //TODO: mabye this can be somehow include in getOn(PhysicalLine). That might be more precise.  
-/*    } else {
+    } else {
         pointTo(cyl); 
     }   
 	
@@ -43,8 +43,8 @@ void Bot::goToLineAndFollowItTo(Cylinder cyl, double distanceToTravel) {
 void Bot::pointTo(Cylinder cyl) {
 	setVel(0);
 	//use trig to figure the angle&distance to cyl
-	double deltaX = cyl.location.x - me.location.x;
-	double deltaY = cyl.location.y - me.location.y;
+	double deltaX = cyl.location.x - location.x;
+	double deltaY = cyl.location.y - location.y;
 	double direction = (DEGREES_PER_RADIAN)*atan(deltaY/deltaX);		//the direction we want to point 
 	if(deltaY < 0) direction = 180 - direction;					//deals with quadrant problems
 
@@ -52,20 +52,34 @@ void Bot::pointTo(Cylinder cyl) {
 
 }
 
+double Bot::getWHL_RADIUS() {
+    return WHL_DIAM / 2;
+}
+
+double Bot::getCOMFY_SPEED() {
+    return 0.5 * MAX_SPEED;
+}
+
+double Bot::getCOMFY_TURN_SPEED() {
+    return 0.5 * MAX_ROT_SPEED;
+}
+
 //These two functions will rely on our charge, our charging speed, time left on the clock, etc. 
 //I will work on what exact inputs they need. -Andrew Elias
+
 //Input: 'time' is in seconds
 void Bot::chargeFor(double time) {
 	wait(time*1000);		//TODO can be more complex than this
 }
 
+//Input: 'time' is in seconds
 void Bot::dischargeFor(double time) {
 	wait (time*1000);		//TODO can be more complex than this
 }
 
 /*
 These four functions return the component of the velocities 
-of the individual wheelsdue to 'velocity' and 'rotVel': */
+of the individual wheels due to 'velocity' and 'rotVel': */
 
 //returns (signed) speed of left wheel (in/sec) due to robot's velocity
 double Bot::getRSpeedDueToVel() {
@@ -109,9 +123,11 @@ void Bot::setVel(double inputVelocity){
 	}
 
 	//physically set it
-	double newLeftSetting  = (velocity + getLSpeedDueToRotVel()) / IN_PER_SEC;
-	double newRightSetting = (velocity + getRSpeedDueToRotVel()) / IN_PER_SEC;
-	if (velocity != oldVel) mci.setVelocity( newLeftSetting, newRightSetting);
+	double newLeftSettingDouble  = (velocity + getLSpeedDueToRotVel()) / IN_PER_SEC;
+	double newRightSettingDouble = (velocity + getRSpeedDueToRotVel()) / IN_PER_SEC;
+    int newLeftSetting = (int)(newLeftSettingDouble + 0.5);
+    int newRightSetting = (int)(newRightSettingDouble + 0.5);
+	if (velocity != oldVel) mci.setVelocity( newLeftSetting, newRightSetting );
 }
 
 double Bot::getRotVel() {
@@ -127,7 +143,7 @@ void Bot::setRotVel(double inputRotVel) {
 		else            inputRotVelSign = -1;
 
 	//don't let the user set it higher than max rot speed
-	if(fabs(inputVelocity) > MAX_ROT_SPEED) {	
+	if(fabs(inputRotVel) > MAX_ROT_SPEED) {	
 		rotVel = MAX_ROT_SPEED * inputRotVelSign;		
 	} else {
 		rotVel = inputRotVel;
@@ -136,7 +152,7 @@ void Bot::setRotVel(double inputRotVel) {
 	//Convert rotational velocity (deg/sec) to linear wheel velocity (in/sec)
 	//note: at this point, rotVel is in degrees per second
 	double rotVelInRadPerSec = rotVel * (RADIANS_PER_DEGREE);    //rotVel in radians per second;
-	double wheelVel = WHL_RADIUS * rotVelInRadPerSec;   //linear velocity of the right(?) wheel due to the rotational velocity; in inches/sec
+	double wheelVel = getWHL_RADIUS() * rotVelInRadPerSec;   //linear velocity of the right(?) wheel due to the rotational velocity; in inches/sec
 
 	//physically set it
 	double newLeftSetting  = (getLSpeedDueToRotVel() - wheelVel) / IN_PER_SEC;
